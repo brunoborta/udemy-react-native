07939b93b3776c5f93633a3dd7226ffb63ef2e2e
Bruno Borta <bruno.bortagaray@objectedge.com>
2019-03-01 18:32:10 -0300

    Adicionado o componente do spinner, mas ainda nao aplicado

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    this.setState({ error: '' });
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -25,12 +39,24 @@ export default class LoginForm extends Component {
             />
         </CardSection>
 
+        <Text style={styles.errorText}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
