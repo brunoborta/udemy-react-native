5c7615450222a2ef6cc524ddec117132079e6006
Bruno Borta <bruno.bortagaray@objectedge.com>
2019-03-12 18:43:03 -0300

    Adicionado o botao add para criar employees

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,5 +1,6 @@
 import firebase from '@firebase/app';
 import '@firebase/auth';
+import { Actions } from 'react-native-router-flux';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -46,4 +47,5 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+  Actions.main();
 }
