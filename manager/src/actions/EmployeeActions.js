c09c09ff3321fdee8ea85594876dea01648e1f13
Bruno Borta <bruno.bortagaray@objectedge.com>
2019-03-15 17:18:27 -0300

    Finalizado o curso de react native e comecando o avancado

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -5,7 +5,9 @@ import '@firebase/auth';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_CLEAR_FORM
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -35,6 +37,35 @@ export const employeesFetch = () => {
     firebase.database().ref(`/users/${currentUser.uid}/employees`)
       .on('value', snapshot => {
         dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val()});
+      });
+  }
+}
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+  
+  return dispatch => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+        Actions.pop();
+      });
+  }
+}
+
+export const employeeClearForm = () => {
+  return { type: EMPLOYEE_CLEAR_FORM }
+}
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+  console.log(uid, currentUser.uid);
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => {
+        Actions.pop();
       });
   }
 }
